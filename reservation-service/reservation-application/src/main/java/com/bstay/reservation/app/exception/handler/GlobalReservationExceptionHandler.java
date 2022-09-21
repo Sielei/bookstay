3e0f005b7e722105f4a7998084ca3592d859015e
Sielei Herman <hsielei@gmail.com>
2022-09-21 20:56:09 +0300

    added a global exception handler for reservation service

--- reservation-service/reservation-application/src/main/java/com/bstay/reservation/app/exception/handler/GlobalReservationExceptionHandler.java
+++ reservation-service/reservation-application/src/main/java/com/bstay/reservation/app/exception/handler/GlobalReservationExceptionHandler.java
@@ -1,4 +1,24 @@
 package com.bstay.reservation.app.exception.handler;
 
-public class GlobalReservationExceptionHandler {
+import com.bstay.common.app.handler.ErrorDto;
+import com.bstay.common.app.handler.GlobalExceptionHandler;
+import com.bstay.reservation.domain.exception.ReservationDomainException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalReservationExceptionHandler extends GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = ReservationDomainException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto handleException(ReservationDomainException reservationDomainException) {
+        return ErrorDto.builder()
+                .errorCode(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .errorMessage(reservationDomainException.getMessage())
+                .build();
+    }
 }

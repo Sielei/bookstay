828e7cb547c2c990733bd47b38f713549a21c746
Sielei Herman <hsielei@gmail.com>
2022-09-21 18:05:49 +0300

    added a post route for creating a new reservation

--- reservation-service/reservation-application/src/main/java/com/bstay/reservation/app/controller/rest/ReservationController.java
+++ reservation-service/reservation-application/src/main/java/com/bstay/reservation/app/controller/rest/ReservationController.java
@@ -1,9 +1,25 @@
 package com.bstay.reservation.app.controller.rest;
 
+import com.bstay.reservation.app.service.dto.CreateReservationCommand;
+import com.bstay.reservation.app.service.dto.CreateReservationResponse;
+import com.bstay.reservation.app.service.ports.input.service.ReservationApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
+
+    private final ReservationApplicationService reservationApplicationService;
+    @PostMapping
+    public ResponseEntity<CreateReservationResponse> createReservationResponse(@RequestBody CreateReservationCommand createReservationCommand){
+        CreateReservationResponse reservationResponse = reservationApplicationService.createReservation(createReservationCommand);
+        return new ResponseEntity<>(reservationResponse, HttpStatus.CREATED);
+    }
 }
